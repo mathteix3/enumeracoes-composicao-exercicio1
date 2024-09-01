e8ef11c28a1ebe6ea95581a938100f61e05c8850
mathteix3 <mathteix3@gmail.com>
2024-09-01 01:51:17 -0300

    Solved Exercise - Enumerations and Composition in Java - UDEMY

---
+++ src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+        System.out.println("Enter worker data: ");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.print("Base salary: ");
+        Double baseSalary = sc.nextDouble();
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+
+        System.out.print("How may contracts to this worker? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            System.out.println("\nEnter contract #" + (i+1) + " data:");
+            System.out.print("Date (DD/MM/YYYY) ");
+            Date contractDate = sdf.parse(sc.next());
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            System.out.print("Duration (hours): ");
+            int hours = sc.nextInt();
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
+
+        System.out.print("\nEnter month and year to calculate income (MM/YYYY) ");
+        String monthAndYears = sc.next();
+        int month = Integer.parseInt(monthAndYears.substring(0,2));
+        int years = Integer.parseInt(monthAndYears.substring(3));
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Department: " + worker.getDepartment().getName());
+        System.out.println("Income for " + monthAndYears + ": " + String.format("%.2f", worker.income(years,month)));
+
+    }
+}
